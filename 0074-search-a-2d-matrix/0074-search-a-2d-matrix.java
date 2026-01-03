0cf31e113b50aae75ec07abf12eb7536e23a5816
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-01-03 06:45:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,12 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int[] row:matrix){
-            for( int col:row){
-                if(col == target){
-                    return true;
-                }
+  
+    //     for(int[] row:matrix){
+    //         for( int col:row){
+    //             if(col == target){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    
+        if(matrix.length==0) return false;
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        int low=0;
+        int high=matrix.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[mid/m][mid%m]==target){ 
+                // [mid/m][mid%m] return the row by dividing and column through the reminder
+                return true;
+            }
+            if(matrix[mid/m][mid%m]<target)
+            {
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return false;
     }
 }
+ 
