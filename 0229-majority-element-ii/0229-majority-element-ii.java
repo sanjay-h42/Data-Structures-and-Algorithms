f9efc9cc57b8daeb22cdc29fb4e514fb13f60f82
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-01-04 09:20:26 +0530

    Time: 2 ms (99.77%), Space: 52.9 MB (20.8%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,50 @@
+//Moore's Voting Algorithm
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int num1=-1,num2=-1,count1=0,count2=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==num1){
+                count1++;
+            }
+            else if(nums[i]==num2){
+                count2++;
+            }
+            else if(count1==0){
+                num1=nums[i];
+                count1=1;
+            }
+            else if(count2==0){
+                num2=nums[i];
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+
+        List<Integer>result=new ArrayList<>();
+         count1=0;
+         count2=0;
+
+         for(int i=0;i<nums.length;i++){
+            if(nums[i]==num1){
+                count1++;
+            }
+            else if(nums[i]==num2){
+                count2++;
+            }
+         }
+
+
+         if(count1>(nums.length/3)){
+            result.add(num1);
+         }
+        if(count2>(nums.length/3)){
+            result.add(num2);
+         }
+
+         return result;
+    }
+}
