dd0871e4ed3e1949b9d953c9de7ac06424fc9ca5
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-01-17 00:39:23 +0530

    Time: 4 ms (51.23%), Space: 44.6 MB (56.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,31 +1,30 @@
 class Solution {
 
     public boolean palindrome(String s) {
-        int left=0;
-        int right=s.length()-1;
-        while(left< right){
-            if(s.charAt(left)!=s.charAt(right)){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
-                left++;
-                right--;          
+            left++;
+            right--;
         }
         return true;
     }
+
     public boolean isPalindrome(String s) {
-        StringBuffer sb=new StringBuffer();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(Character.isDigit(ch) || Character.isLetter(ch)){
-                sb.append(ch);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
             }
         }
-            String result=sb.toString();result=result.toLowerCase();
-            if(palindrome(result))
-               { 
-                return true;
-               }
-        
-        return false;
+
+        return palindrome(sb.toString());
     }
 }
