bcd4082daf4e3ded78eb7ca6f233cb6fcb44b62b
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-01-18 10:08:15 +0530

    Time: 9 ms (16.54%), Space: 44.2 MB (5.72%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,15 +1,28 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int s_sum=0,t_sum=0;
-        for(int i=0;i<s.length();i++){
-                s_sum+=s.charAt(i);
+        // int s_sum=0,t_sum=0;
+        // for(int i=0;i<s.length();i++){
+        //         s_sum+=s.charAt(i);
+        // }
+
+        // for(int i=0;i<t.length();i++){
+        //         t_sum+=t.charAt(i);
+        // }
+
+        // int result=t_sum-s_sum;
+        // return (char)result;
+
+        List<Character> li = new LinkedList<>();
+
+        for (char a : s.toCharArray()) {
+            li.add(a);
         }
 
-        for(int i=0;i<t.length();i++){
-                t_sum+=t.charAt(i);
+        for (char a : t.toCharArray()) {
+            if (!li.remove((Character) a)) {
+                return a;
+            }
         }
-
-        int result=t_sum-s_sum;
-        return (char)result;
+        return ' ';
     }
 }
