a644555b14b8bc83b63b14f83ab954ec08875707
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-01-25 23:47:04 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.41%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb=new StringBuilder();
+        int i=0;
+        while(i<command.length()){
+            if(command.charAt(i)=='G'){
+                sb.append("G");
+                i=i+1;
+            }
+            else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                sb.append("o");
+                i=i+2;
+            }
+            else{
+                sb.append("al");
+                i=i+4;
+            }
+        }
+        return sb.toString();
+    }
+}
