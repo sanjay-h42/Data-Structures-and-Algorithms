0bd71fa37e028e9a8a9a1831b5b6694da118115d
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-01-31 07:00:26 +0530

    Time: 1 ms (100%), Space: 46.8 MB (9.28%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
@@ -1,11 +1,18 @@
 class Solution {
     public int vowelStrings(String[] words, int left, int right) {
         int count=0;
-        for(int i=left ; i<=right ; i++){
-            if( (words[i].charAt(0)=='a' || words[i].charAt(0)=='e' || words[i].charAt(0)=='i' || words[i].charAt(0)=='o' || words[i].charAt(0)=='u') && (words[i].charAt(words[i].length()-1)=='a' || words[i].charAt(words[i].length()-1)=='e' || words[i].charAt(words[i].length()-1)=='i' || words[i].charAt(words[i].length()-1)=='o' || words[i].charAt(words[i].length()-1)=='u') ){
-                count++;
-            }
+     for(int i=left;i<=right;i++){
+        String word=words[i];
+        char first=word.charAt(0);
+        char last=word.charAt(word.length()-1);
+
+        if(isVowel(first)&& isVowel(last)){
+            count++;
         }
-        return count;
+     }
+     return count;
+    }
+    private boolean isVowel(char ch){
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
 }
