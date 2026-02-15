b6a9a8fecb5f7ed8f55bb3e2a173604571a85784
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-02-15 15:36:54 +0530

    Time: 0 ms (100%), Space: 42.8 MB (5.89%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        long nOdd=(n+1)/2;
+        long nEven=n/2;
+        long mOdd=(m+1)/2;
+        long mEven=m/2;
+
+        return nOdd * mEven + nEven *mOdd;
+    }
+}
