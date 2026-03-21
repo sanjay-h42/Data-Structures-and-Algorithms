d732b1c65dc5ecb34448f9ba2ccef33e5afa6a5e
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-03-22 00:30:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,11 +2,8 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer>set=new HashSet<>();
         for(int n:nums){
-            if(set.contains(n)){
-                return true;
-            }
             set.add(n);
         }
-        return false;
+        return set.size()<nums.length;
     }
 }
