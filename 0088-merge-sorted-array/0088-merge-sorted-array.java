b83f5c9f10df6ffe579b47ca9f31c13898ebbd72
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-07-07 15:37:04 +0530

    Time: 0 ms (100%), Space: 43.8 MB (45.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,27 +1,22 @@
-//using comparing last index of nums1[] and first index of the nums2[]
-//use shell or gap method
-// using two pointer
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
         int j=n-1;
+        int i=m-1;
         int k=m+n-1;
-
         while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-            }
-            else{
-                nums1[k]=nums2[j];
-                j--;
-            }
-            k--;
+           if(nums1[i]<nums2[j]){
+            nums1[k--]=nums2[j--];  
+           }
+
+           else{
+            nums1[k--]=nums1[i--];
+           }   
         }
 
         while(j>=0){
-            nums1[k--]=nums2[j--];
+            {
+                nums1[k--]=nums2[j--];
+            }
         }
     }
-    
 }
