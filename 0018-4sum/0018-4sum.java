6f8cfd31d321833e7cb9c64c02e7d5f933fc70b4
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-07-13 16:35:05 +0530

    Time: 20 ms (65.7%), Space: 45.6 MB (77.46%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,41 +1,42 @@
-// use two pointer and binary search i and j ,front and back move to and forth 
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result=new ArrayList<>();
-        if(nums==null || nums.length==0) return result;
-        int n=nums.length;
         Arrays.sort(nums);
+       List<List<Integer>>Result=new ArrayList<>();
+      for(int i=0;i<nums.length-3;i++){
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                long target_2=(long) target-nums[j]-nums[i];
-                int front=j+1;
-                int back=n-1;
-
-                while(front < back){
-                    long two_sum= (long) nums[front]+nums[back];
-
-                    if(two_sum < target_2) front++;
-                    else if(two_sum > target_2) back--;
-                    else{
-                        List<Integer> quad=new ArrayList<>();
-                        quad.add(nums[i]);
-                        quad.add(nums[j]);
-                        quad.add(nums[front]);
-                        quad.add(nums[back]);
-                        result.add(quad);
-                        
-                        //processing for the duplicating for the number 3
-                        while(front < back && nums[front]==quad.get(2)) front++;
-                        //processing for the duplicating for the number 4
-                         while(front < back && nums[back]==quad.get(3)) back--;
-                    }
-                }
-                while(j+1 < n && nums[j+1]==nums[j]) j++;
-            }
-            while(i+1 < n && nums[i+1]==nums[i]) i++;
+        if(i>0 && nums[i]==nums[i-1]){
+            continue;
         }
-        return result;
+      for(int j=i+1;j<nums.length-2;j++){
+        if(j>i+1 && nums[j]==nums[j-1]){
+            continue;
+        }
+        int left=j+1;
+        int right=nums.length-1;
+        
+        while(left<right){
+        long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
+         if(sum==target){
+            Result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+            left++;
+            while(left<right && nums[left]==nums[left-1]){
+                left++;
+            }
+            right--;
+            while(left<right && nums[right]==nums[right+1]){
+                right--;
+            }
+         }
+         else if(sum<target){
+            left++;
+         }
+         else{
+            right--;
+         }
+        }
+      }
+
+      }
+        return Result;
     }
 }
