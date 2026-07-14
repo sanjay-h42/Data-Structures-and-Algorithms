269a03d4a57aa30d6417c2444346d7d6acdf02ee
Sanjay Supaiya <sanjaysupaiya@gmail.com>
2026-07-14 14:52:13 +0530

    Time: 0 ms (100%), Space: 43.6 MB (43.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int left=0,mid=0,right=nums.length-1;
+
+        while(mid<=right){
+            if(nums[mid]==0){
+                int temp=nums[left];
+                nums[left]=nums[mid];
+                nums[mid]=temp;
+                left++;
+                mid++;
+            }
+
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[right];
+                nums[right]=temp;
+                right--;
+            }
+
+        }
+        return ;
     }
 }
